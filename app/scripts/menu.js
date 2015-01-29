7c8b8fab5337a6044fed7f39a0711ddba12c16c4
John Albritton <jtareb1@att.net>
2015-01-29 17:03:24 -0500

    array

---
+++ app/scripts/menu.js
@@ -0,0 +1,609 @@
+{
+	"BREAKFAST": [
+		{
+		"id": 15,
+		"item": "Bagel & Egg",
+		"price": 2,
+		"description": "A Bagel and an Egg",
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 18,
+		"item": "Biscuit & Egg",
+		"price": 2,
+		"description": "A magnificent Biscuit filled with scrambled Egg",
+		"allergies": 1,
+		"favorite": 1,
+		"spicy": 1,
+		"vegan": 0
+		},
+		{
+		"id": 19,
+		"item": "Bacon Biscuit",
+		"price": 3,
+		"description": "Succulent Bacon stuffed into a Biscuit",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 1
+		},
+		{
+		"id": 22,
+		"item": "Chicken Biscuit",
+		"price": 3,
+		"description": "Tasty Chicken stuffed Biscuit",
+		"allergies": 0,
+		"favorite": 1,
+		"spicy": 0,
+		"vegan": 1
+		},
+		{
+		"id": 23,
+		"item": "Ham Biscuit",
+		"price": 3,
+		"description": "Delicious Ham on a Biscuit",
+		"allergies": 0,
+		"favorite": 1,
+		"spicy": 0,
+		"vegan": 0 
+		},	
+		{
+		"id": 24,
+		"item": "Turkey Bacon Biscuit",
+		"price": 3,
+		"description": "Nothing better than Turkey and Bacon on a Biscuit",
+		"allergies": 0,
+		"favorite": 1,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 25,
+		"item": "French Toast",
+		"price": 6,
+		"description": "Toast made in France",
+		"allergies": 1,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 28,
+		"item": "Salmon Croquettes",
+		"price": 6,
+		"description": "Salmon on a Croquette",
+		"allergies": 0,
+		"favorite": 1,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 33,
+		"item": "Chicken & Waffles",
+		"price": 8,
+		"description": "like it says, Chicken and Waffles",
+		"allergies": 0,
+		"favorite": 1,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 4,
+		"item": "French Toast Combo",
+		"price": 8,
+		"description": "French Toast, 2 eggs, hash browns, 2pc bacon/sausage",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+	],
+
+"SANDWICHES": [
+	
+
+		{
+		"id": 9,
+		"item": "Peantbutter & Jelly",
+		"price": 4,
+		"description": "Peanutbutter with Jelly and/or Bananas & Honey",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 3,
+		"item": "Egg Salad",
+		"price": 4,
+		"description": "Egg Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 6,
+		"item": "Vera's Fried Beef Lettuce Slaw",
+		"price": 8,
+		"description": "",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 65,
+		"item": "Honey Maple Ham",
+		"price": 8,
+		"description": "Honey Maple Ham",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 66,
+		"item": "Lemon Pepper Chicken",
+		"price": 8,
+		"description": "Lemon Pepper Chicken",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 67,
+		"item": "Spicey Turkey",
+		"price": 8,
+		"description": "Spicey Turkey",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 68,
+		"item": "Chicken Salad",
+		"price": 8,
+		"description": "Chicken Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 69,
+		"item": "Tuna Salad",
+		"price": 8,
+		"description": "Tuna Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 70,
+		"item": "Chicken Sausage",
+		"price": 8,
+		"description": "Chicken Sausage with coleslaw",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 69,
+		"item": "London Broil",
+		"price": 10,
+		"description": "London Broil",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+
+	],
+
+
+"TOPPINGS & EXTRAS": [
+		{
+		"id": 70,
+		"item": "Cheese",
+		"price": 1,
+		"description": "Cheese",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 71,
+		"item": "Bacon",
+		"price": 1,
+		"description": "Bacon",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 72,
+		"item": "Avocado",
+		"price": 2,
+		"description": "Avacado",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 73,
+		"item": "Chicken",
+		"price": 3,
+		"description": "Chicken",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+	],
+
+	"SIDES": [
+	
+		{
+		"id": 74,
+		"item": "Biscuit with Gravy",
+		"price": 2,
+		"description": "Biscuit with Gravy",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 75,
+		"item": "Grits",
+		"price": 2,
+		"description": "Grits",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		}
+		{
+		"id": 76,
+		"item": "Hash Browns",
+		"price": 2,
+		"description": "Hash Browns",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0,
+		},
+		{
+		"id": 77,
+		"item": "Fruit Bowl",
+		"price": 3,
+		"description": "Fruit Bowl",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+	],
+
+"SALADS": [
+		{
+		"id": 78,
+		"item": "Side Salad",
+		"price": 3,
+		"description": "Side Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 79,
+		"item": "House Salad",
+		"price": 6,
+		"description": "House Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 80,
+		"item": "Chicken Caesar Salad",
+		"price": 7,
+		"description": "Chiken Caesar Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 81,
+		"item": "Chef Salad",
+		"price": 7,
+		"description": "Chef Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 82,
+		"item": "Grilled Chicken Salad",
+		"price": 7,
+		"description": "Grilled Chicken Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 83,
+		"item": "Chopped Salad",
+		"price": 9,
+		"description": "Chopped Salad with Turkey,Ham, and Pastrami",
+		"allergies": 0
+		"favorite": 0,,
+		"spicy": 0,
+		"vegan": 0
+		},
+		
+
+"SOUP OF THE DAY": [
+
+		{
+		"id": 84,
+		"item": "Baked Potato",
+		"price": {
+			"cup": 3,
+			"bowl": 5
+		},
+		"description": "Baked Potato",
+		"allergies": 0
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 85,
+		"item": "Broccoli & Cheddar",
+		"price": {
+			"cup": 3,
+			"bowl": 5
+		},
+		"description": "Broccoli & Cheddar",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 86,
+		"item": "Chicken Noodle",
+		"price": {
+			"cup": 3,
+			"bowl": 5,
+		},
+		"description": "Chicken Noodle",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 88,
+		"item": "Tortilla",
+		"price": {
+			"cup": 3,
+			"bowl": 5
+		},
+		"description": "Tortilla",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 89,
+		"item": "Turkey Chili",
+		"price": {
+			"cup": 3,
+			"bowl": 5
+
+		},
+		"description": "Turkey Chili",
+		"allergies": 0
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+
+],
+
+"DRINKS": [
+		{
+		"id": 90
+		"item": "Bottled Water",
+		"price": 1,
+		"description": "Bottled Water",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 91,
+		"item": "Canned Soda",
+		"price": 1,
+		"description": "Canned Soda",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 92,
+		"item": "Peach Tea",
+		"price": 2,
+		"description": "Peach Tea",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 93,
+		"item": "Sweet Tea",
+		"price": 2,
+		"description": "Sweet Tea",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 94,
+		"item": "Coffee",
+		"price": {
+			"small": 1,
+			"large:": 2
+
+		},
+		"description": "Coffee",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 95,
+		"item": "Espresso",
+		"price": 4.5,
+		"description": "Espresso",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+],
+
+"Desserts": [
+
+		{
+		"id": 96,
+		"item": "(3) Chocolate Chip/Oatmeal Raisin Cookies",
+		"price": 1,
+		"description": "Chocolate Chip/Oatmeal Raisin Cookies",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 97,
+		"item": "Banana Pudding",
+		"price": 4,
+		"description": "Banana Pudding",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 98,
+		"item": "Bread Pudding",
+		"price": 4,
+		"description": "Bread Pudding",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 99,
+		"item": "Cheesecake",
+		"price": 4,
+		"description": "Cheesecake",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 100,
+		"item": "Key Lime Pie",
+		"price": 4,
+		"description": "Key Lime Pie",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+
+
+
+],
+
+"VERA'S DESSERTS": {
+		"id": 101,
+		"item": "Red Velvet Cupcakes",
+		"price": 4,
+		"description":, "Red Velvet Cupcakes",
+		"allergies": 0
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+
+		},
+		{
+		"id": 102,
+		"item": "Sweet Potato Pie",
+		"price": 4,
+		"description": "Sweet Potato Pie",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 103,
+		"item": "Pecan Pie",
+		"price": 4,
+		"description": "Pecan Pie",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+
+		"id": 104,
+		"item": "Buttermilk Pie",
+		"price": 4,
+		"description": "Buttermilk Pie",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+],
+
+}
+
+
+
+	
+
+
+
+
+
+
