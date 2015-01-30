5932093304b4f66fd2a8f5b8b454d5d32cb67661
John Albritton <jtareb1@att.net>
2015-01-30 15:49:58 -0500

    fri late commit

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,12 +1,696 @@
+var menu = {
+	"breakfast": [
+		{
+		"id": 001,
+		"item": "Bagel & Egg",
+		"price": 2,
+		"description": "A Bagel and an Egg",
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},		
+		{
+		"id": 002,
+		"item": "Biscuit & Egg",
+		"price": 2,
+		"description": "A magnificent Biscuit filled with scrambled Egg",
+		"allergies": 1,
+		"favorite": 1,
+		"spicy": 1,
+		"vegan": 0
+		},		
+		{
+		"id": 003,
+		"item": "Bacon Biscuit",
+		"price": 3,
+		"description": "Succulent Bacon stuffed into a Biscuit",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 1
+		},		
+		{
+		"id": 004,
+		"item": "Chicken Biscuit",
+		"price": 3,
+		"description": "Tasty Chicken stuffed Biscuit",
+		"allergies": 0,
+		"favorite": 1,
+		"spicy": 0,
+		"vegan": 1
+		},		
+		{
+		"id": 005,
+		"item": "Ham Biscuit",
+		"price": 3,
+		"description": "Delicious Ham on a Biscuit",
+		"allergies": 0,
+		"favorite": 1,
+		"spicy": 0,
+		"vegan": 0 
+		},		
+		{
+		"id": 006,
+		"item": "Turkey Bacon Biscuit",
+		"price": 3,
+		"description": "Nothing better than Turkey and Bacon on a Biscuit",
+		"allergies": 0,
+		"favorite": 1,
+		"spicy": 0,
+		"vegan": 0
+		},		
+		{
+		"id": 007,
+		"item": "French Toast",
+		"price": 6,
+		"description": "Toast made in France",
+		"allergies": 1,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},		
+		{
+		"id": 008,
+		"item": "Salmon Croquettes",
+		"price": 6,
+		"description": "Salmon on a Croquette",
+		"allergies": 0,
+		"favorite": 1,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 009,
+		"item": "Chicken & Waffles",
+		"price": 8,
+		"description": "like it says, Chicken and Waffles",
+		"allergies": 0,
+		"favorite": 1,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 010,
+		"item": "French Toast Combo",
+		"price": 8,
+		"description": "French Toast, 2 eggs, hash browns, 2pc bacon/sausage",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+	],
 
-var storyText = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam facilis blanditiis dolores mollitia obcaecati nemo, at dolorem a neque quasi, eum minus exercitationem odio rem sequi quae! Nam libero, nulla.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, cum doloribus ea sapiente totam voluptatum, ratione itaque, alias voluptatibus hic asperiores, blanditiis veniam! Nobis consectetur itaque pariatur fugiat assumenda alias!</p>';
+"sandwiches": [	
+
+		{
+		"id": 011,
+		"item": "Peantbutter & Jelly",
+		"price": 4,
+		"description": "Peanutbutter with Jelly and/or Bananas & Honey",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 012,
+		"item": "Egg Salad",
+		"price": 4,
+		"description": "Egg Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 013,
+		"item": "Vera's Fried Beef Lettuce Slaw",
+		"price": 8,
+		"description": "",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 014,
+		"item": "Honey Maple Ham",
+		"price": 8,
+		"description": "Honey Maple Ham",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 015,
+		"item": "Lemon Pepper Chicken",
+		"price": 8,
+		"description": "Lemon Pepper Chicken",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 016,
+		"item": "Spicey Turkey",
+		"price": 8,
+		"description": "Spicey Turkey",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 017,
+		"item": "Chicken Salad",
+		"price": 8,
+		"description": "Chicken Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 018,
+		"item": "Tuna Salad",
+		"price": 8,
+		"description": "Tuna Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 019,
+		"item": "Chicken Sausage",
+		"price": 8,
+		"description": "Chicken Sausage with coleslaw",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 020,
+		"item": "London Broil",
+		"price": 10,
+		"description": "London Broil",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+
+	],
+
+
+"toppings": [
+		{
+		"id": 021,
+		"item": "Cheese",
+		"price": 1,
+		"description": "Cheese",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 022,
+		"item": "Bacon",
+		"price": 1,
+		"description": "Bacon",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 023,
+		"item": "Avocado",
+		"price": 2,
+		"description": "Avacado",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 024,
+		"item": "Chicken",
+		"price": 3,
+		"description": "Chicken",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+	],
+
+	"sides": [
+	
+		{
+		"id": 025,
+		"item": "Biscuit with Gravy",
+		"price": 2,
+		"description": "Biscuit with Gravy",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 026,
+		"item": "Grits",
+		"price": 2,
+		"description": "Grits",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 027,
+		"item": "Hash Browns",
+		"price": 2,
+		"description": "Hash Browns",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0,
+		},
+		{
+		"id": 028,
+		"item": "Fruit Bowl",
+		"price": 3,
+		"description": "Fruit Bowl",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		}
+	],
+
+"salads": [
+		{
+		"id": 029,
+		"item": "Side Salad",
+		"price": 3,
+		"description": "Side Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 030,
+		"item": "House Salad",
+		"price": 6,
+		"description": "House Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 031,
+		"item": "Chicken Caesar Salad",
+		"price": 7,
+		"description": "Chiken Caesar Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 032,
+		"item": "Chef Salad",
+		"price": 7,
+		"description": "Chef Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 033,
+		"item": "Grilled Chicken Salad",
+		"price": 7,
+		"description": "Grilled Chicken Salad",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 034,
+		"item": "Chopped Salad",
+		"price": 9,
+		"description": "Chopped Salad with Turkey,Ham, and Pastrami",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		}
+	],
+		
+
+"soups": [
+
+		{
+		"id": 035,
+		"item": "Baked Potato",
+		"price": {
+			"cup": 3,
+			"bowl": 5
+		},
+		"description": "Baked Potato",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 036,
+		"item": "Broccoli & Cheddar",
+		"price": {
+			"cup": 3,
+			"bowl": 5
+		},
+		"description": "Broccoli & Cheddar",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 037,
+		"item": "Chicken Noodle",
+		"price": {
+			"cup": 3,
+			"bowl": 5,
+		},
+		"description": "Chicken Noodle",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 038,
+		"item": "Tortilla",
+		"price": {
+			"cup": 3,
+			"bowl": 5
+		},
+		"description": "Tortilla",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 039,
+		"item": "Turkey Chili",
+		"price": {
+			"cup": 3,
+			"bowl": 5
+
+		},
+		"description": "Turkey Chili",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+
+],
+
+"drinks": [
+		{
+		"id": 040,
+		"item": "Bottled Water",
+		"price": 1,
+		"description": "Bottled Water",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 041,
+		"item": "Canned Soda",
+		"price": 1,
+		"description": "Canned Soda",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 042,
+		"item": "Peach Tea",
+		"price": 2,
+		"description": "Peach Tea",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 043,
+		"item": "Sweet Tea",
+		"price": 2,
+		"description": "Sweet Tea",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 044,
+		"item": "Coffee",
+		"price": {
+			"small": 1,
+			"large:": 2
+			},
+		"description": "Coffee",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 045,
+		"item": "Espresso",
+		"price": 4.5,
+		"description": "Espresso",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+],
+
+"desserts": [
+
+		{
+		"id": 046,
+		"item": "(3) Chocolate Chip Cookies",
+		"price": 1,
+		"description": "Chocolate Chip/Oatmeal Raisin Cookies",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 047,
+		"item": "(3) Oatmeal Raisin Cookies",
+		"price": 1,
+		"description": "Chocolate Chip/Oatmeal Raisin Cookies",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 048,
+		"item": "Banana Pudding",
+		"price": 4,
+		"description": "Banana Pudding",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 049,
+		"item": "Bread Pudding",
+		"price": 4,
+		"description": "Bread Pudding",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 050,
+		"item": "Cheesecake",
+		"price": 4,
+		"description": "Cheesecake",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 051,
+		"item": "Key Lime Pie",
+		"price": 4,
+		"description": "Key Lime Pie",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+
+],
+
+"veraDesserts": [
+		{		
+		"id": 052,
+		"item": "Red Velvet Cupcakes",
+		"price": 4,
+		"description": "Red Velvet Cupcakes",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+
+		},
+		{
+		"id": 053,
+		"item": "Sweet Potato Pie",
+		"price": 4,
+		"description": "Sweet Potato Pie",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 054,
+		"item": "Pecan Pie",
+		"price": 4,
+		"description": "Pecan Pie",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+		{
+		"id": 055,
+		"item": "Buttermilk Pie",
+		"price": 4,
+		"description": "Buttermilk Pie",
+		"allergies": 0,
+		"favorite": 0,
+		"spicy": 0,
+		"vegan": 0
+		},
+]
+
+}
 
 
 
-console.log('Hello from John');
+	
+
+
+
+
+
+
+
+
+var storyText = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam facilis blanditiis dolores mollitia obcaecati nemo, at dolorem a neque quasi, eum minus exercitationem odio rem sequi quae! Nam libero, nulla.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, cum doloribus ea sapiente totam voluptatum, ratione itaque, alias voluptatibus hic asperiores, blanditiis veniam! Nobis consectetur itaque pariatur fugiat assumenda alias! <a href="#" class="collapse">Show Less</a></p>';
+
 
 
 _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
+
+// DECLARE AND ASSIGN VARIABLES
+
+var breakfast = menu.breakfast;
+var sandwiches = menu.sandwiches;
+var toppings = menu.toppings;
+var sides = menu.sides;
+var salads = menu.salads;
+var soups = menu.soups;
+var drinks = menu.drinks;
+var desserts = menu.desserts;
+var veraDesserts = menu.veraDesserts;
+
+// MENU TEMPLATE
+
+breakfast.forEach(function (food) {
+$('.breakfast-items').append(_.template($('#menu-template').text())(food));	
+});
+
+sandwiches.forEach(function (food) {
+$('.sandwich-items').append(_.template($('#menu-template').text())(food));
+});
+
+salads.forEach(function (food) {
+$('.salad-items').append(_.template($('#menu-template').text())(food));
+});
+
+soups.forEach(function (food) {
+$('.soup-items').append(_.template($('#soups-template').text())(food));
+});
+
+drinks.forEach(function (food) {
+	$('.drink-items').append(_.template($('#menu-template').text())(food));
+});
+
+// drinks.forEach(function (food) {
+// 	if (food.item = "Coffee") { 
+// 		$('.drink-items').append(_.template($('#coffee-template').text())(food));
+// } else {
+// 		$('.drink-items').append(_.template($('#menu-template').text())(food));
+// });
+
+toppings.forEach(function (food) {
+$('.topping-items').append(_.template($('#menu-template').text())(food));
+});
+
+desserts.forEach(function (food) {
+$('.dessert-items').append(_.template($('#menu-template').text())(food));
+});
+
+veraDesserts.forEach(function (food) {
+$('.v-dessert-items').append(_.template($('#menu-template').text())(food));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -14,47 +698,64 @@ _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
 _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
 
 
+var fullNewsPost,
+    shortStoryText,
+    latestNewsTemplate;
+
 var clampText = function (text, chars) {
-  chars = chars || 115;
-  return text.slice(0, chars) + '&hellip; <a href="#">Read More</a>';
+  chars = chars || 100;
+  return text.slice(0, chars) + '&hellip; <a href="#" class="expand">Show More</a>';
 };
 
-$('.story-leader').prepend(clampText(storyText, 160));
+// Add story text.
+shortStoryText = clampText(storyText, 160);
+$('.story div').append(shortStoryText);
 
-var fullNewsPost;
 
 // Snag the template from the markup and convert to a function that can
 // accept input data.
-var latestNewsTemplate = _.template($('#latestNewsTemplate').html());
+latestNewsTemplate = _.template($('#latestNewsTemplate').html());
 
-// Request latest news data from API and receive a promise back...
+// Request latest news data from API and receive a promise back.
+// Act on whatever is returned from the promise.
 $.getJSON('http://private-anon-eafb492e3-restaurantapi.apiary-mock.com/news/latest')
-
-// Run a spinner setTimeout here...
-.always()
-
-// Promise resolution successful...
 .done(function (data) {
-
-  fullNewsPost = data.post;
-  data.post = clampText(data.post, 105);
-
+  fullNewsPost = data.post + ' <a href="#" class="collapse">Show Less</a>';
+  shortNewsPost = data.post = clampText(data.post, 105);
   $('article.news h2').after(latestNewsTemplate(data));
-  // Turn off the spinner
 })
-
-// Promise resolution failed...
 .fail(function (jqXHR) {
   var msg = 'Error retrieving data from server. ';
   $('article.news').prepend(msg);
   console.log(msg + jqXHR.statusText);
-  // Turn off the spinner
 });
 
 
-$('.blog').on('click', 'article a', function (e) {
+// Expand article if "Show More" is clicked.
+$('.blog').on('click', 'article a.expand', function (e) {
   e.preventDefault();
-  console.log(e);
-  console.log(e.target.parentNode);
+  var isStory = $(e.target).parents().filter('article').hasClass('story');
+  if (isStory) {
+    $('article.story p').html(storyText);
+  } else {
+    $('article.news p').html(fullNewsPost);
+  }
 });
+
+
+// // Collapse article if "Show Less" is clicked.
+// $('.blog').on('click', 'article a.collapse', function (e) {
+//   e.preventDefault();
+//   var isStory = $(e.target).parents().filter('article').hasClass('story');
+//   if (isStory) {
+//     $('article.story p').html(shortStoryText);
+//   } else {
+//     $('article.news p').html(shortNewsPost);
+//   }
+// });
+
+
+
+
+
 
