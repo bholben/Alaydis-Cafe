230cc2f84053424ec4f26cbfb313ed61fbcd194e
Bob Holben <bholben@gmail.com>
2015-01-29 21:40:27 -0500

    Link up story_text.js story data

---
+++ app/scripts/story_text.js
@@ -0,0 +1,3 @@
+
+var storyText = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam facilis blanditiis dolores mollitia obcaecati nemo, at dolorem a neque quasi, eum minus exercitationem odio rem sequi quae! Nam libero, nulla.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, cum doloribus ea sapiente totam voluptatum, ratione itaque, alias voluptatibus hic asperiores, blanditiis veniam! Nobis consectetur itaque pariatur fugiat assumenda alias!</p>';
+
