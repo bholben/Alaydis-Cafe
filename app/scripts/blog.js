c20a9ebb8a890970a3a8a2334a90babc1ba0a358
Bob Holben <bholben@gmail.com>
2015-02-01 14:16:31 -0500

    Pull js in to separate functional files

--- app/scripts/blog.js
+++ app/scripts/blog.js
@@ -1,8 +1,4 @@
 
-// Change template engine settings to use {{ }} syntax.
-_.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
-
-
 var fullNewsPost,
     shortStoryText,
     latestNewsTemplate;
@@ -59,52 +55,4 @@ $('.blog').on('click', 'article a.expand', function (e) {
 //     $('article.news p').html(shortNewsPost);
 //   }
 // });
-
-var reservationEndPoint = 'http://tiy-atl-fe-server.herokuapp.com/collections/alaydis',
-    reservationForm = $('#reservationsForm'),
-    confirmReservation = $('.confirm-reservation');
-
-reservationForm.on('submit', function (e) {
-  e.preventDefault();
-
-  var form = $(this),
-      formData = {
-        fullName: form.find('input[name="fullName"]').val(),
-        numParty: form.find('input[name="numParty"]').val(),
-        date: form.find('input[name="date"]').val(),
-        notes: form.find('textarea[name="notes"]').val(),
-        seatingPref: form.find('select[name="seatingPref"]').val(),
-      };
-      form[0].reset();
-
-  $.post(reservationEndPoint, formData)
-  .done(function (data) {
-    confirmReservation
-      .removeClass('hide')
-      .addClass('success-text')
-      .text('Reservation confirmed.  See you soon!');
-  })
-  .fail(function (jqXHR) {
-    var msg = 'Communication or data error with server. ';
-    confirmReservation
-      .removeClass('hide')
-      .addClass('error-text')
-      .text(msg);
-    console.error(msg + jqXHR.statusText);
-  });
-
-});
-
-var flyoutLeft = $('.flyout-left'),
-    handleLeft = $('.flyout-left .handle');
-
-handleLeft.on('click', function () {
-  flyoutLeft.toggleClass('flyout-left-open');
-  if (flyoutLeft.hasClass('flyout-left-open')) {
-    handleLeft.html('<span>R</span><i class="fa fa-chevron-left"></i>');
-  } else {
-    handleLeft.html('<span>R</span><i class="fa fa-chevron-right"></i>');
-  };
-});
-
 
