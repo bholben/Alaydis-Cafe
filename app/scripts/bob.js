36f6d68d39766a0e4fcff4debba8a83800a9a395
Bob Holben <bholben@gmail.com>
2015-02-01 13:01:36 -0500

    Redo flyouts

--- app/scripts/bob.js
+++ app/scripts/bob.js
@@ -90,5 +90,17 @@ $('#reservationsForm').submit(function (e) {
 
 });
 
+var flyoutLeft = $('.flyout-left'),
+    handleLeft = $('.flyout-left .handle');
+
+handleLeft.on('click', function () {
+  console.log('Hi');
+  flyoutLeft.toggleClass('flyout-left-open');
+  if (flyoutLeft.hasClass('flyout-left-open')) {
+    handleLeft.html('<span>R</span><i class="fa fa-chevron-left"></i>');
+  } else {
+    handleLeft.html('<span>R</span><i class="fa fa-chevron-right"></i>');
+  };
+});
 
 
